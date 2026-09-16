721d7b647a3e1fe3628d57e79646f49a16ad34a6
Danilu Silva <dan7zada@gmail.com>
2026-09-16 11:26:06 -0300

    feat: adicionar RequestUpdateBookJson para atualização parcial de livros

---
+++ GerenciadorDeLivraria/Communication/Requests/RequestUpdateBookJson.cs
@@ -0,0 +1,22 @@
+﻿using GerenciadorDeLivraria.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorDeLivraria.Communication.Requests;
+
+public class RequestUpdateBookJson
+{
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "Título do livro deve ter no mínimo 2 e no máximo 120 caracteres")]
+    public string? Titulo { get; set; } = string.Empty;
+
+    [StringLength(120, MinimumLength = 2, ErrorMessage = "O campo 'Autor' deve ter no mínimo 2 e no máximo 120 caracteres")]
+    public string? Autor { get; set; } = string.Empty;
+
+    [GeneroValido]
+    public string? Genero { get; set; } = string.Empty;
+
+    [Range(0, double.MaxValue, ErrorMessage = "Preço não pode ser negativo!")]
+    public decimal? Preco { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Estoque não pode ser negativo!")]
+    public int? Estoque { get; set; }
+}
